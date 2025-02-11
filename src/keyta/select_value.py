d6edf851bde1d0b050687176e6d06bec028ac059
Marduk Bolaños <marduk.bolanos@imbus.de>
2025-02-11 13:07:19 +0100

    Use Optional instead of Type|None

--- src/keyta/select_value.py
+++ src/keyta/select_value.py
@@ -1,13 +1,14 @@
 import json
 from dataclasses import dataclass, asdict
+from typing import Optional
 
 
 @dataclass
 class SelectValue:
-    arg_name: str|None
-    kw_call_index: int|None
-    pk: int|None
-    user_input: str|None
+    arg_name: Optional[str]
+    kw_call_index: Optional[int]
+    pk: Optional[int]
+    user_input: Optional[str]
 
     def jsonify(self):
         # sort the keys in order to always generate the same string
