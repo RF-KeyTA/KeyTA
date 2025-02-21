b7adeefd19624a0dba54321f9af81d0b3980d349
Marduk Bolaños <marduk.bolanos@imbus.de>
2025-02-21 22:00:24 +0100

    Added comments explaining why these models are needed

--- src/keyta/apps/keywords/admin/window_keyword_parameter.py
+++ src/keyta/apps/keywords/admin/window_keyword_parameter.py
@@ -5,6 +5,8 @@ from keyta.admin.base_admin import BaseAdmin
 from ..models import WindowKeywordParameter
 
 
+# This admin is necessary for the delete field
+#  in the Parameters inline to work
 @admin.register(WindowKeywordParameter)
 class WindowKeywordParameterAdmin(BaseAdmin):
     pass
--- src/keyta/apps/keywords/admin/window_keyword_return_value.py
+++ src/keyta/apps/keywords/admin/window_keyword_return_value.py
@@ -5,6 +5,8 @@ from keyta.admin.base_admin import BaseAdmin
 from ..models import WindowKeywordReturnValue
 
 
+# This admin is necessary for the delete field
+# in the Return Values inline to work
 @admin.register(WindowKeywordReturnValue)
 class WindowKeywordReturnValueAdmin(BaseAdmin):
     pass
