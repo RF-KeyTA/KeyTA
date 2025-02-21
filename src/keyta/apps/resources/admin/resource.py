f07b6450af7b4ea76886bbb6625979c2987c5b1b
Marduk Bolaños <marduk.bolanos@imbus.de>
2025-02-21 21:50:34 +0100

    Extracted the Keywords inline to a separate file

--- src/keyta/apps/resources/admin/resource.py
+++ src/keyta/apps/resources/admin/resource.py
@@ -10,23 +10,8 @@ from django.utils.translation import gettext as _
 from keyta.admin.base_admin import BaseAdmin
 from keyta.rf_import.import_resource import import_resource
 
-from ..models import Resource, ResourceKeyword
-
-
-class Keywords(admin.TabularInline):
-    model = ResourceKeyword
-    fields = ['name', 'short_doc']
-    readonly_fields = ['name', 'short_doc']
-    extra = 0
-    can_delete = False
-    show_change_link = True
-    verbose_name_plural = _('Schlüsselwörter')
-
-    def has_add_permission(self, request, obj=None):
-        return False
-
-    def has_change_permission(self, request, obj=None):
-        return False
+from ..models import Resource
+from .keywords_inline import Keywords
 
 
 @admin.register(Resource)
