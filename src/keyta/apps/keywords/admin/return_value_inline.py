87cd635b97df301f331509d72f868b2da45caf11
Marduk Bolaños <marduk.bolanos@imbus.de>
2025-02-26 13:53:12 +0100

    Use a BaseSelect widget in the ReturnValue inline

--- src/keyta/apps/keywords/admin/return_value_inline.py
+++ src/keyta/apps/keywords/admin/return_value_inline.py
@@ -1,7 +1,8 @@
-from django.db.models import QuerySet, Q
+from django.db.models import Q
 from django.utils.translation import gettext as _
 
 from keyta.admin.base_inline import TabularInlineWithDelete
+from keyta.widgets import BaseSelect
 
 from ..models import KeywordReturnValue, Keyword
 
@@ -13,22 +14,27 @@ class ReturnValueInline(TabularInlineWithDelete):
     max_num = 1
 
     def get_formset(self, request, obj=None, **kwargs):
-        formset = super().get_formset(request, obj, **kwargs)
         keyword: Keyword = obj
-        queryset: QuerySet = formset.form.base_fields['kw_call_return_value'].queryset
+        formset = super().get_formset(request, obj, **kwargs)
+        kw_call_return_value_field = formset.form.base_fields['kw_call_return_value']
         return_values = (
-            queryset
+            kw_call_return_value_field.queryset
             .filter(keyword_call__in=keyword.calls.all())
             .exclude(Q(name__isnull=True) & Q(return_value__isnull=True))
         )
-        formset.form.base_fields['kw_call_return_value'].queryset = return_values
+        kw_call_return_value_field.queryset = return_values
 
         if return_values.exists():
-            formset.form.base_fields['kw_call_return_value'].widget.attrs.update({
-                'data-placeholder': _('Rückgabewert auswählen')
-            })
+            kw_call_return_value_field.widget = BaseSelect(
+                _('Rückgabewert auswählen')
+            )
         else:
-            formset.form.base_fields['kw_call_return_value'].disabled = True
-            formset.form.base_fields['kw_call_return_value'].empty_label = _('Keine Rückgabewerte aus den Schritten')
+            if return_value := keyword.return_value.first():
+                return_value.delete()
+
+            kw_call_return_value_field.disabled = True
+            kw_call_return_value_field.widget = BaseSelect(
+                _('Keine Rückgabewerte aus den Schritten')
+            )
 
         return formset
