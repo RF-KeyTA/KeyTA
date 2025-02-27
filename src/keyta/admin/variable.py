5553ad40972312487b09b458f69e2f34020d7970
Marduk Bolaños <marduk.bolanos@imbus.de>
2025-02-27 17:44:51 +0100

    Factored out base admin classes in the variables app to keyta.admin

--- src/keyta/admin/variable.py
+++ src/keyta/admin/variable.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib import admin
 from django.db.models.functions import Lower
+from django.forms import HiddenInput
 from django.http import HttpRequest
 from django.utils.translation import gettext as _
 
@@ -8,6 +9,7 @@ from adminsortable2.admin import SortableAdminBase
 
 from keyta.forms import form_with_select
 from keyta.models.variable import AbstractVariable
+from keyta.widgets import BaseSelect
 
 from apps.variables.models import (
     Variable,
@@ -187,3 +189,73 @@ class SchemaFields(TabularInlineWithDelete):
 class BaseVariableSchemaAdmin(BaseAdmin):
     fields = ['name']
     inlines = [SchemaFields]
+
+
+class BaseVariableSchemaQuickAddAdmin(BaseAdmin):
+    fields = ['windows', 'name']
+    inlines = [SchemaFields]
+
+    def formfield_for_manytomany(self, db_field, request, **kwargs):
+        field = super().formfield_for_manytomany(db_field, request, **kwargs)
+
+        if db_field.name == 'windows':
+            field = forms.ModelMultipleChoiceField(
+                field.queryset,
+                widget=forms.MultipleHiddenInput
+            )
+
+        return field
+
+
+class BaseVariableQuickAddAdmin(BaseAdmin):
+    fields = ['systems', 'windows', 'name', 'schema', 'type']
+    form = form_with_select(
+        VariableQuickAdd,
+        'schema',
+        _('Schema auswählen')
+    )
+
+    def formfield_for_manytomany(self, db_field, request, **kwargs):
+        field = super().formfield_for_manytomany(db_field, request, **kwargs)
+
+        if db_field.name in {'systems', 'windows'}:
+            field = forms.ModelMultipleChoiceField(
+                field.queryset,
+                widget=forms.MultipleHiddenInput
+            )
+
+        return field
+
+    def formfield_for_dbfield(self, db_field, request: HttpRequest, **kwargs):
+        field = super().formfield_for_dbfield(db_field, request, **kwargs)
+
+        if db_field.name == 'type':
+            field = forms.ChoiceField(
+                choices=field.choices,
+                widget=BaseSelect(_('Variablentyp auswählen'))
+            )
+
+        if request.GET.get('list_id', None):
+            if db_field.name in {'schema', 'type'}:
+                field.widget = HiddenInput()
+
+        return field
+
+    def save_model(self, request: HttpRequest, obj, form, change):
+        super().save_model(request, obj, form, change)
+
+        if not change:
+            variable: Variable = obj
+
+            if list_id := request.GET.get('list_id', None):
+                VariableInList.objects.create(
+                    list_variable=Variable.objects.get(id=list_id),
+                    variable=variable
+                )
+
+            if variable.schema:
+                for field in variable.schema.fields.all():
+                    VariableValue.objects.create(
+                        variable=variable,
+                        name=field.name
+                    )
--- src/keyta/apps/variables/admin.py
+++ src/keyta/apps/variables/admin.py
@@ -1,22 +1,21 @@
-from django import forms
 from django.contrib import admin
-from django.forms import HiddenInput
-from django.http import HttpRequest
-from django.utils.translation import gettext as _
 
-from forms import form_with_select
 from keyta.admin.base_admin import BaseAdmin
-from keyta.admin.variable import BaseVariableAdmin, BaseVariableSchemaAdmin, SchemaFields
-from widgets import BaseSelect
+from keyta.admin.variable import (
+    BaseVariableAdmin,
+    BaseVariableQuickAddAdmin,
+    BaseVariableSchemaAdmin,
+    BaseVariableSchemaQuickAddAdmin,
+)
 
 from .models import (
     Variable,
+    VariableInList,
     VariableQuickAdd,
     VariableSchema,
     VariableSchemaField,
     VariableSchemaQuickAdd,
     VariableValue,
-    VariableInList,
     VariableWindowRelation
 )
 
@@ -47,75 +46,13 @@ class VariableWindowAdmin(BaseAdmin):
 
 
 @admin.register(VariableQuickAdd)
-class VariableQuickAddAdmin(BaseAdmin):
-    fields = ['systems', 'windows', 'name', 'schema', 'type']
-    form = form_with_select(
-        VariableQuickAdd,
-        'schema',
-        _('Schema auswählen')
-    )
-
-    def formfield_for_manytomany(self, db_field, request, **kwargs):
-        field = super().formfield_for_manytomany(db_field, request, **kwargs)
-
-        if db_field.name in {'systems', 'windows'}:
-            field = forms.ModelMultipleChoiceField(
-                field.queryset,
-                widget=forms.MultipleHiddenInput
-            )
-
-        return field
-
-    def formfield_for_dbfield(self, db_field, request: HttpRequest, **kwargs):
-        field = super().formfield_for_dbfield(db_field, request, **kwargs)
-
-        if db_field.name == 'type':
-            field = forms.ChoiceField(
-                choices=field.choices,
-                widget=BaseSelect(_('Variablentyp auswählen'))
-            )
-
-        if request.GET.get('list_id', None):
-            if db_field.name in {'schema', 'type'}:
-                field.widget = HiddenInput()
-
-        return field
-
-    def save_model(self, request: HttpRequest, obj, form, change):
-        super().save_model(request, obj, form, change)
-
-        if not change:
-            variable: Variable = obj
-
-            if list_id := request.GET.get('list_id', None):
-                VariableInList.objects.create(
-                    list_variable=Variable.objects.get(id=list_id),
-                    variable=variable
-                )
-
-            if variable.schema:
-                for field in variable.schema.fields.all():
-                    VariableValue.objects.create(
-                        variable=variable,
-                        name=field.name
-                    )
+class VariableQuickAddAdmin(BaseVariableQuickAddAdmin):
+    pass
 
 
 @admin.register(VariableSchemaQuickAdd)
-class VariableSchemaQuickAddAdmin(BaseAdmin):
-    fields = ['windows', 'name']
-    inlines = [SchemaFields]
-
-    def formfield_for_manytomany(self, db_field, request, **kwargs):
-        field = super().formfield_for_manytomany(db_field, request, **kwargs)
-
-        if db_field.name == 'windows':
-            field = forms.ModelMultipleChoiceField(
-                field.queryset,
-                widget=forms.MultipleHiddenInput
-            )
-
-        return field
+class VariableSchemaQuickAddAdmin(BaseVariableSchemaQuickAddAdmin):
+    pass
 
 
 @admin.register(VariableInList)
