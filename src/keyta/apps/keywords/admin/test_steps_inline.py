782ac723306e24a235d47703174594620bed183e
Marduk Bolaños <marduk.bolanos@imbus.de>
2025-02-27 15:15:31 +0100

    In the TestSteps first show the sequences and then the actions

--- src/keyta/apps/keywords/admin/test_steps_inline.py
+++ src/keyta/apps/keywords/admin/test_steps_inline.py
@@ -31,7 +31,7 @@ class TestStepsInline(
         formset = super().get_formset(request, obj, **kwargs)
         formset.form.base_fields['window'].queryset = windows
         formset.form.base_fields['to_keyword'].queryset = (
-            Keyword.objects.actions() | Keyword.objects.sequences()
+            Keyword.objects.sequences() | Keyword.objects.actions()
         )
 
         return formset
