4ee3569f9be61918d94eef77f3c8c445fe00f667
Marduk Bolaños <marduk.bolanos@imbus.de>
2025-02-28 15:44:05 +0100

    Do not depend on a migration from an app not under our control

--- src/keyta/apps/keywords/migrations/0012_remove_keywordcallparametersource_kw_call_parameter_source_sum_type_and_more.py
+++ src/keyta/apps/keywords/migrations/0012_remove_keywordcallparametersource_kw_call_parameter_source_sum_type_and_more.py
@@ -7,7 +7,6 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('variables', '0010_alter_variable_type'),
         ('keywords', '0011_keywordcall_variable'),
     ]
 
