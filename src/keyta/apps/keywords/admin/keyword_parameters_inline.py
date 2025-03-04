92ccd437efdd96efe3bce73beb844090eaf63e8e
Marduk Bolaños <marduk.bolanos@imbus.de>
2025-03-04 12:04:59 +0100

    Set the width of the name field of Keyword Parameters to 100%

--- src/keyta/apps/keywords/admin/keyword_parameters_inline.py
+++ src/keyta/apps/keywords/admin/keyword_parameters_inline.py
@@ -11,3 +11,11 @@ class ParametersInline(SortableTabularInlineWithDelete):
     extra = 0
     verbose_name = _('Parameter')
     verbose_name_plural = _('Parameters')
+
+    def formfield_for_dbfield(self, db_field, request, **kwargs):
+        field = super().formfield_for_dbfield(db_field, request, **kwargs)
+
+        if db_field.name == 'name':
+            field.widget.attrs.update({'style': 'width: 100%'})
+
+        return field
