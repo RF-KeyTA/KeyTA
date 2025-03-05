b6845183882a12f1aefc1bca1a10decdfd435f62
Marduk Bolaños <marduk.bolanos@imbus.de>
2025-03-05 17:18:32 +0100

    The migration should only depend on the initial migration of the variables app

--- src/keyta/apps/keywords/migrations/0014_alter_keywordcall_variable.py
+++ src/keyta/apps/keywords/migrations/0014_alter_keywordcall_variable.py
@@ -7,7 +7,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('variables', '0012_variablevalue_schema_field_alter_variable_schema_and_more'),
+        ('variables', '0001_initial'),
         ('keywords', '0013_alter_keywordcallparametersource_type'),
     ]
 
