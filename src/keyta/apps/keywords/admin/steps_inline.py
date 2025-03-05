6864ad260e516bb18866c119f064f4b16c0741a2
Marduk Bolaños <marduk.bolanos@imbus.de>
2025-03-05 20:08:28 +0100

    The keyword steps now have 0 extra forms

--- src/keyta/apps/keywords/admin/steps_inline.py
+++ src/keyta/apps/keywords/admin/steps_inline.py
@@ -1,7 +1,4 @@
-from django.utils.translation import gettext as _
-
 from keyta.admin.base_inline import DeleteRelatedField, SortableTabularInline
-
 
 from ..forms import StepsForm
 from ..models import KeywordCall
@@ -19,4 +16,4 @@ class StepsInline(
     fk_name = 'from_keyword'
     fields = ['to_keyword']
     form = StepsForm
-    extra = 1  # Must be > 0 in order for django-select2 to work
+    extra = 0
