d5e30b9fb8602ff03abf3d0d07804827095ae906
Marduk Bolaños <marduk.bolanos@imbus.de>
2025-03-06 12:08:02 +0100

    The documentation field does not have a label (or description)

--- src/keyta/admin/field_documentation.py
+++ src/keyta/admin/field_documentation.py
@@ -1,12 +1,11 @@
 from django.contrib import admin
 from django.utils.safestring import mark_safe
-from django.utils.translation import gettext as _
 
 
 class DocumentationField:
     fields = ['readonly_documentation']
     readonly_fields = ['readonly_documentation']
 
-    @admin.display(description=_('Dokumentation'))
+    @admin.display(description='')
     def readonly_documentation(self, obj):
         return mark_safe(obj.documentation)
