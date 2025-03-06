7d3ebb6c8dc5f0907fe56f2d117f4ea01331a352
Marduk Bolaños <marduk.bolanos@imbus.de>
2025-03-06 08:59:16 +0100

    A KeywordCall can now have several return values

--- src/keyta/apps/keywords/admin/keywordcall_return_value_inline.py
+++ src/keyta/apps/keywords/admin/keywordcall_return_value_inline.py
@@ -8,8 +8,7 @@ from ..models import KeywordCallReturnValue
 class KeywordCallReturnValueInline(BaseTabularInline):
     model = KeywordCallReturnValue
     fields = ['name']
-    extra = 1
-    max_num = 1
+    extra = 0
     verbose_name = _('Rückgabewert')
     verbose_name_plural = _('Rückgabewert')
     can_delete = False
