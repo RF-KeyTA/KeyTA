2b6c5e5d87822972948e57a47abfd2ce7bc7b641
Marduk Bolaños <marduk.bolanos@imbus.de>
2025-03-11 23:21:05 +0100

    It is not necessary to define a custom Formset for the steps inline.

--- src/keyta/apps/keywords/admin/steps_inline.py
+++ src/keyta/apps/keywords/admin/steps_inline.py
@@ -1,24 +1,9 @@
-from adminsortable2.admin import CustomInlineFormSet
-
 from keyta.admin.base_inline import DeleteRelatedField, SortableTabularInline
 
 from ..forms import StepsForm
 from ..models import KeywordCall
 from .field_first_parameter import FirstParameterField
 from .field_keywordcall_args import KeywordCallArgsField
-
-
-class StepsFormset(CustomInlineFormSet):
-    def add_fields(self, form, index):
-        super().add_fields(form, index)
-
-        kw_call: KeywordCall = form.instance
-
-        # The index of an extra form is None
-        if index is not None and kw_call.pk:
-            # A keyword from a Resource cannot be edited
-            if kw_call.to_keyword.resource:
-                form.fields['to_keyword'].widget.can_change_related = False
 
 
 class StepsInline(
@@ -31,5 +16,4 @@ class StepsInline(
     fk_name = 'from_keyword'
     fields = ['to_keyword']
     form = StepsForm
-    formset = StepsFormset
     extra = 0
