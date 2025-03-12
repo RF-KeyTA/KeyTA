83c84122228e1272d7b43c9f034c1cc9fb1675d8
Marduk Bolaños <marduk.bolanos@imbus.de>
2025-03-12 18:05:54 +0100

    In the Keywords inline of a library show a link to each keyword

--- src/keyta/apps/libraries/admin/keywords_inline.py
+++ src/keyta/apps/libraries/admin/keywords_inline.py
@@ -1,17 +1,18 @@
+from django.contrib import admin
 from django.utils.translation import gettext as _
 
 from keyta.admin.base_inline import BaseTabularInline
+from keyta.widgets import link
 
 from ..models import LibraryKeyword
 
 
 class Keywords(BaseTabularInline):
     model = LibraryKeyword
-    fields = ['name', 'short_doc']
-    readonly_fields = ['name', 'short_doc']
+    fields = ['name_link', 'short_doc']
+    readonly_fields = ['name_link', 'short_doc']
     extra = 0
     can_delete = False
-    show_change_link = True
     verbose_name_plural = _('Schlüsselwörter')
 
     def has_add_permission(self, request, obj=None):
@@ -19,3 +20,10 @@ class Keywords(BaseTabularInline):
 
     def has_change_permission(self, request, obj=None):
         return False
+
+    @admin.display(description=_('Name'))
+    def name_link(self, keyword: LibraryKeyword):
+        return link(
+            keyword.get_admin_url(),
+            keyword.name
+        )
