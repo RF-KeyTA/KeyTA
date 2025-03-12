40d1306ca7965287d74b87a347b9bb0ac4b73ff7
Marduk Bolaños <marduk.bolanos@imbus.de>
2025-03-12 18:40:06 +0100

    Deleted unused models ResourceKeyword and ResourceKeywordDocumentation

--- src/keyta/apps/resources/models/__init__.py
+++ src/keyta/apps/resources/models/__init__.py
@@ -1,3 +1,2 @@
 from keyta.apps.resources.models.resource import Resource
-from keyta.apps.resources.models.resource_keyword import ResourceKeyword, ResourceKeywordDocumentation
-from keyta.apps.resources.models.resource_import import ResourceImport, ResourceImportType
+from keyta.apps.resources.models.resource_import import ResourceImport
