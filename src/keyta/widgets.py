74ecda28e08921ef0d4ce8fb85e57317216387da
Marduk Bolanos <marduk.bolanos@imbus.de>
2025-03-18 14:08:43 +0100

    Override create_option in BaseSelect

--- src/keyta/widgets.py
+++ src/keyta/widgets.py
@@ -94,6 +94,16 @@ class BaseSelect(forms.Select):
 
         super().__init__(default_attrs, choices)
 
+    def create_option(
+        self, name, value, label, selected, index, subindex=None, attrs=None
+    ):
+        option = super().create_option(name, value, label, selected, index, subindex, attrs)
+
+        if value is None:
+            option['attrs'].update({'disabled': 'true'})
+
+        return option
+
     @property
     def i18n_name(self):
         """Name of the i18n file for the current language."""
@@ -142,35 +152,6 @@ class BaseSelectMultiple(BaseSelect):
 
 class KeywordCallParameterSelect(BaseSelect):
     template_name = 'admin/keywordcall/select.html'
-
-    def create_option(
-        self, name, value, label, selected, index, subindex=None, attrs=None
-    ):
-        index = str(index) if subindex is None else "%s_%s" % (index, subindex)
-        option_attrs = (
-            self.build_attrs(self.attrs, attrs) if self.option_inherits_attrs else {}
-        )
-
-        if value is None:
-            option_attrs.update({'disabled': 'true'})
-
-        if selected:
-            option_attrs.update(self.checked_attribute)
-
-        if "id" in option_attrs:
-            option_attrs["id"] = self.id_for_label(option_attrs["id"], index)
-
-        return {
-            "name": name,
-            "value": value,
-            "label": label,
-            "selected": selected,
-            "index": index,
-            "attrs": option_attrs,
-            "type": self.input_type,
-            "template_name": self.option_template_name,
-            "wrap_label": True,
-        }
 
 
 class ModelSelect2AdminWidget(ModelSelect2Widget):
