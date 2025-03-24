1acd804d39af6a92f8c7cfc1194d042cef835d52
Marduk Bolanos <marduk.bolanos@imbus.de>
2025-03-24 09:43:03 +0100

    Updated the translations

--- src/keyta/project/settings.py
+++ src/keyta/project/settings.py
@@ -174,7 +174,8 @@ USE_I18N = True
 USE_TZ = True
 
 LOCALE_PATHS = [
-    BASE_DIR / "locale"
+    BASE_DIR / "locale",
+    BASE_DIR / "model_clone_locale"
 ]
 
 # Static files (CSS, JavaScript, Images)
