07d4bcc97430e9c43fbc2432a629124e6f1891cd
Marduk Bolaños <marduk.bolanos@imbus.de>
2025-03-26 11:54:40 +0100

    In the LibraryImport admin: - if it is a LibraryImport from an Action show the link to the Action that imports the Library - if it is a LibraryImport from an Execution show the init documentation of the Library and the parameters inline

--- src/keyta/apps/libraries/admin/library_import.py
+++ src/keyta/apps/libraries/admin/library_import.py
@@ -1,3 +1,4 @@
+from django import forms
 from django.contrib import admin
 from django.utils.translation import gettext as _
 
@@ -10,11 +11,33 @@ from .library_import_parameters_inline import LibraryImportParametersInline
 
 @admin.register(LibraryImport)
 class LibraryImportAdmin(BaseAdmin):
-    fields = ['library_init_doc']
-    readonly_fields = ['library_init_doc']
-    inlines = [
-        LibraryImportParametersInline
-    ]
+    def get_fields(self, request, obj=None):
+        return self.get_readonly_fields(request, obj)
+
+    def get_form(self, request, obj=None, change=False, **kwargs):
+        return forms.modelform_factory(
+            LibraryImport,
+            fields=['keyword'],
+            labels = {
+                'keyword': _('Aktion')
+            }
+        )
+
+    def get_inlines(self, request, obj):
+        library_import: LibraryImport = obj
+        
+        if library_import.execution:
+            return [LibraryImportParametersInline]
+        
+        return []
+
+    def get_readonly_fields(self, request, obj=None):
+        library_import: LibraryImport = obj
+
+        if library_import.execution:
+            return ['library_init_doc']
+        
+        return ['keyword']
 
     @admin.display(description=_('Dokumentation'))
     def library_init_doc(self, obj: LibraryImport):
