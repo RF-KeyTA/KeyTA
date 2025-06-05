ae9f60277fc78c80b0272a7d83ba2f4c1b293a05
Marduk Bolaños <marduk.bolanos@imbus.de>
2025-06-05 11:22:09 +0200

    In the admin of KeywordCallReturnValue only show the string representation of the return value

--- src/keyta/apps/keywords/admin/keywordcall_return_value.py
+++ src/keyta/apps/keywords/admin/keywordcall_return_value.py
@@ -8,4 +8,12 @@ from ..models import KeywordCallReturnValue
 # This admin is necessary in order to delete a KeywordCallReturnValue
 @admin.register(KeywordCallReturnValue)
 class KeywordCallReturnValueAdmin(BaseAdmin):
-    pass
+    fields = ['return_value_name']
+    fields = ['return_value_name']
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    @admin.display(description='Name')
+    def return_value_name(self, kw_call_return_value: KeywordCallReturnValue):
+        return str(kw_call_return_value)
