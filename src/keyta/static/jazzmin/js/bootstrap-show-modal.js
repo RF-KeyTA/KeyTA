30aa3e807acaabdd4102cc26540b57f1def43cf4
Marduk Bolanos <marduk.bolanos@imbus.de>
2025-06-10 14:45:59 +0200

    Removed the "back" link in the upper left corner of modals

--- src/keyta/static/jazzmin/js/bootstrap-show-modal.js
+++ src/keyta/static/jazzmin/js/bootstrap-show-modal.js
@@ -67,7 +67,6 @@
         this.element.innerHTML = '<div class="modal-dialog ' + this.props.modalDialogClass + '" role="document">' +
             '<div class="modal-content">' +
             '<div class="modal-header">' +
-            goBack() +
             reload() +
             '<h5 class="modal-title"></h5>' +
             '<button type="button" class="close" data-dismiss="modal" aria-label="Close">' +
