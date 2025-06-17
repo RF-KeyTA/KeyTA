4359ed30ea8698b6c92b15925678563a2219f4c9
Marduk Bolaños <marduk.bolanos@imbus.de>
2025-06-17 08:40:29 +0200

    Fixed type hints

--- src/keyta/apps/variables/models.py
+++ src/keyta/apps/variables/models.py
@@ -52,8 +52,8 @@ class Variable(AbstractBaseModel):
 
     def add_value(
             self,
-            schema_field: 'AbstractVariableSchemaField',
-            list_variable: Optional['AbstractVariable']=None
+            schema_field: 'VariableSchemaField',
+            list_variable: Optional['Variable']=None
     ):
         VariableValue = apps.get_model('variables', 'VariableValue')
         VariableValue.objects.get_or_create(
