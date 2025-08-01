63ed411bf858b59515176989c63e935a29bccf40
Marduk Bolanos <marduk.bolanos@imbus.de>
2025-08-01 15:06:37 +0200

    Sequence Steps inline: Use a quick change widget for the to_keyword field

--- src/keyta/apps/sequences/admin/steps_inline.py
+++ src/keyta/apps/sequences/admin/steps_inline.py
@@ -9,6 +9,7 @@ from keyta.apps.keywords.forms import StepsForm
 from keyta.apps.keywords.models import Keyword, KeywordCall
 from keyta.apps.windows.models import Window
 from keyta.forms import form_with_select
+from keyta.widgets import quick_change_widget
 
 from ..models import Sequence
 
@@ -75,11 +76,14 @@ class SequenceSteps(StepsInline):
             for resource, keywords in groups
         ]
 
-        formset.form.base_fields['to_keyword'].choices = (
-            [(None, None)] +
-            window_actions +
-            resource_kws +
-            global_actions(sequence.systems.all())
+        to_keyword_field = formset.form.base_fields['to_keyword']
+        to_keyword_field.choices = (
+                [(None, None)] +
+                window_actions +
+                resource_kws +
+                global_actions(sequence.systems.all())
         )
+        to_keyword_field.widget = quick_change_widget(to_keyword_field.widget)
+
 
         return formset
