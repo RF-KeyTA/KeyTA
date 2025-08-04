168ab12926332bc47ff53805f1d976ac273c8ad5
Marduk Bolanos <marduk.bolanos@imbus.de>
2025-08-04 09:33:01 +0200

    When filtering by Window show the choice "Window-independent" first

--- src/keyta/admin/list_filters.py
+++ src/keyta/admin/list_filters.py
@@ -41,3 +41,31 @@ class WindowListFilter(DependentListFilter):
 
         self.empty_value_display = _('Masken-unabhängig')
         self.title = _('Maske')
+
+    def choices(self, changelist):
+        yield {
+            "selected": self.lookup_val is None and not self.lookup_val_isnull,
+            "query_string": changelist.get_query_string(
+                remove=[self.lookup_kwarg, self.lookup_kwarg_isnull]
+            ),
+            "display": _("All"),
+        }
+
+        # Show the empty choice first
+        if self.include_empty_choice:
+            yield {
+                "selected": bool(self.lookup_val_isnull),
+                "query_string": changelist.get_query_string(
+                    {self.lookup_kwarg_isnull: "True"}, [self.lookup_kwarg]
+                ),
+                "display": self.empty_value_display,
+            }
+
+        for pk_val, val in self.lookup_choices:
+            yield {
+                "selected": self.lookup_val == str(pk_val),
+                "query_string": changelist.get_query_string(
+                    {self.lookup_kwarg: pk_val}, [self.lookup_kwarg_isnull]
+                ),
+                "display": val,
+            }
