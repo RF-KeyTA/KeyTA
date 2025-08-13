beab5b2ac77d69b39199f84e74f56ad82d3fec24
Marduk Bolanos <marduk.bolanos@imbus.de>
2025-08-13 08:55:30 +0200

    Steps inline: Show the first three parameters of a keyword call

---
+++ src/keyta/apps/keywords/admin/field_parameters.py
@@ -0,0 +1,35 @@
+from django.contrib import admin
+from django.utils.safestring import mark_safe
+
+from ..models import KeywordCall
+
+
+class ParameterFields:
+    def show_parameter(self, kw_call: KeywordCall, position: int):
+        params = list(kw_call.parameters.all())
+        position0 = position - 1
+
+        if len(params) > position0:
+            param = params[position0]
+            value = params[position0].current_value or ''
+            return mark_safe('%s<br><i style="color: gray">%s</i>' % (value, str(param)))
+        else:
+            return ''
+
+    @admin.display(description='')
+    def parameter1(self, kw_call: KeywordCall):
+        return self.show_parameter(kw_call, 1)
+
+    @admin.display(description='')
+    def parameter2(self, kw_call: KeywordCall):
+        return self.show_parameter(kw_call, 2)
+
+    @admin.display(description='')
+    def parameter3(self, kw_call: KeywordCall):
+        return self.show_parameter(kw_call, 3)
+
+    def get_fields(self, request, obj=None):
+        return super().get_fields(request, obj) + ['parameter1', 'parameter2', 'parameter3']
+
+    def get_readonly_fields(self, request, obj=None):
+        return super().get_readonly_fields(request, obj) + ['parameter1', 'parameter2', 'parameter3']
--- src/keyta/apps/keywords/admin/steps_inline.py
+++ src/keyta/apps/keywords/admin/steps_inline.py
@@ -2,14 +2,14 @@ from keyta.admin.base_inline import DeleteRelatedField, SortableTabularInline
 
 from ..forms import StepsForm
 from ..models import KeywordCall
-from .field_first_parameter import FirstParameterField
+from .field_parameters import ParameterFields
 from .field_keywordcall_args import KeywordCallArgsField
 
 
 class StepsInline(
     DeleteRelatedField,
-    FirstParameterField, 
-    KeywordCallArgsField, 
+    ParameterFields,
+    KeywordCallArgsField,
     SortableTabularInline
 ):
     model = KeywordCall
