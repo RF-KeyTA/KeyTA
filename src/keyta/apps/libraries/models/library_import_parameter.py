63e8341c58731174f96557ddf836b775bf0a5cf1
Marduk Bolanos <marduk.bolanos@imbus.de>
2025-08-14 12:03:55 +0200

    Fixed import

--- src/keyta/apps/libraries/models/library_import_parameter.py
+++ src/keyta/apps/libraries/models/library_import_parameter.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
-from models.base_model import AbstractBaseModel
+from keyta.models.base_model import AbstractBaseModel
 
 
 class LibraryImportParameter(AbstractBaseModel):
