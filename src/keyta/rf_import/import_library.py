e9b90baff5e10b7b45a03a0172529d226da4a939
Marduk Bolanos <marduk.bolanos@imbus.de>
2025-08-14 12:36:10 +0200

    When replacing the links of init_doc explicitly convert init_doc to a string

--- src/keyta/rf_import/import_library.py
+++ src/keyta/rf_import/import_library.py
@@ -52,7 +52,7 @@ def import_library(name: str):
                 init_arg.delete()
 
     lib.import_keywords(libdoc_dict, typedocs)
-    lib.init_doc = lib.replace_links(lib.init_doc, typedocs)
+    lib.init_doc = lib.replace_links(str(lib.init_doc), typedocs)
     lib.documentation = lib.replace_links(lib.documentation, typedocs)
     lib.save()
 
