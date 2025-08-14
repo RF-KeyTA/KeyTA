03823fe4555ad9d9cf8314b39127b5c5246685f1
Marduk Bolanos <marduk.bolanos@imbus.de>
2025-08-14 10:37:40 +0200

    LibraryImportParametersInline: Added the type hint for request

--- src/keyta/apps/libraries/admin/library_import_parameters_inline.py
+++ src/keyta/apps/libraries/admin/library_import_parameters_inline.py
@@ -1,4 +1,5 @@
 from django.db.models import QuerySet
+from django.http import HttpRequest
 from django.utils.translation import gettext_lazy as _
 
 from keyta.admin.base_inline import BaseTabularInline
@@ -10,14 +11,14 @@ from ..models import LibraryImportParameter
 class LibraryImportParametersInline(BaseTabularInline):
     model = LibraryImportParameter
     fields = ['name', 'value']
-    formset = LibraryParameterFormSet
     readonly_fields = ['name']
+    formset = LibraryParameterFormSet
     extra = 0
     max_num = 0
     verbose_name_plural = _('Einstellungen')
     can_delete = False
 
-    def get_queryset(self, request):
+    def get_queryset(self, request: HttpRequest) -> QuerySet:
         queryset: QuerySet = super().get_queryset(request)
         return queryset.filter(user=request.user)
 
