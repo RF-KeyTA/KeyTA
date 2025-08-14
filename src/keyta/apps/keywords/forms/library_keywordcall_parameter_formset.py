b3e83399da7f9557f050847d2a7d9eefa4bc65f2
Marduk Bolanos <marduk.bolanos@imbus.de>
2025-08-14 19:52:48 +0200

    LibraryKeywordCallParameterFormset: In add_fields added the condition if kw_call_parameter.pk so that this function also works for var args

--- src/keyta/apps/keywords/forms/library_keywordcall_parameter_formset.py
+++ src/keyta/apps/keywords/forms/library_keywordcall_parameter_formset.py
@@ -30,82 +30,83 @@ class LibraryKeywordCallParameterFormset(UserInputFormset):
         )
 
         kw_call_parameter: KeywordCallParameter = form.instance
-        if param_typedoc := kw_call_parameter.parameter.typedoc:
-            type_list: list = json.loads(param_typedoc)
+        if kw_call_parameter.pk:
+            if param_typedoc := kw_call_parameter.parameter.typedoc:
+                type_list: list = json.loads(param_typedoc)
 
-            if type_list == ['bool']:
-                choices = [
-                    (JSONValue.user_input('True'), 'True'),
-                    (JSONValue.user_input('False'), 'False'),
-                ]
+                if type_list == ['bool']:
+                    choices = [
+                        (JSONValue.user_input('True'), 'True'),
+                        (JSONValue.user_input('False'), 'False'),
+                    ]
 
-                form.fields['value'] = DynamicChoiceField(
-                    widget=BaseSelect(
-                        '',
-                        choices=choices
-                    )
-                )
-            else:
-                choices = dict()
-                enable_user_input = False
-
-                for type_ in type_list:
-                    if type_ == 'None':
-                        choices['None'] = [
-                            (JSONValue.user_input('None'), 'None')
-                        ]
-                    elif type_ == 'bool':
-                        choices['bool'] = [
-                            (JSONValue.user_input('True'), 'True'),
-                            (JSONValue.user_input('False'), 'False'),
-                        ]
-                    elif type_ in self.typedocs:
-                        typedoc = self.typedocs[type_]
-
-                        if typedoc['type'] == 'Enum':
-                            enum_name = typedoc['name']
-                            choices[enum_name] = dict()
-
-                            sorted_members = sorted(typedoc['members'])
-                            members = (
-                                list(filter(lambda x: x[0].isalpha(), sorted_members)) +
-                                list(filter(lambda x: not x[0].isalpha(), sorted_members))
-                            )
-                            for member in members:
-                                if member.lower() not in {'true', 'false'}:
-                                    choices[enum_name][JSONValue.user_input(member)] = member
-
-                            choices[enum_name] = list(choices[enum_name].items())
-                        else:
-                            enable_user_input = True
-                    else:
-                        enable_user_input = True
-
-                if enable_user_input:
-                    if choices:
-                        enum_values = {
-                            key
-                            for value in list(choices.values())
-                            for key in invert_dictionary(dict(value))
-                        }
-                        json_value, value = user_input
-
-                        if value in enum_values:
-                            user_input = None, _('Kein Wert')
-
-                    form.fields['value'] = user_input_field(
-                        _('Wert auswählen oder eintragen'),
-                        user_input,
-                        choices=list(choices.items()) + self.ref_choices
+                    form.fields['value'] = DynamicChoiceField(
+                        widget=BaseSelect(
+                            '',
+                            choices=choices
+                        )
                     )
                 else:
-                    if choices:
-                        form.fields['value'] = DynamicChoiceField(
-                            widget=BaseSelect(
-                                '',
-                                choices=list(choices.items())
-                            )
+                    choices = dict()
+                    enable_user_input = False
+
+                    for type_ in type_list:
+                        if type_ == 'None':
+                            choices['None'] = [
+                                (JSONValue.user_input('None'), 'None')
+                            ]
+                        elif type_ == 'bool':
+                            choices['bool'] = [
+                                (JSONValue.user_input('True'), 'True'),
+                                (JSONValue.user_input('False'), 'False'),
+                            ]
+                        elif type_ in self.typedocs:
+                            typedoc = self.typedocs[type_]
+
+                            if typedoc['type'] == 'Enum':
+                                enum_name = typedoc['name']
+                                choices[enum_name] = dict()
+
+                                sorted_members = sorted(typedoc['members'])
+                                members = (
+                                    list(filter(lambda x: x[0].isalpha(), sorted_members)) +
+                                    list(filter(lambda x: not x[0].isalpha(), sorted_members))
+                                )
+                                for member in members:
+                                    if member.lower() not in {'true', 'false'}:
+                                        choices[enum_name][JSONValue.user_input(member)] = member
+
+                                choices[enum_name] = list(choices[enum_name].items())
+                            else:
+                                enable_user_input = True
+                        else:
+                            enable_user_input = True
+
+                    if enable_user_input:
+                        if choices:
+                            enum_values = {
+                                key
+                                for value in list(choices.values())
+                                for key in invert_dictionary(dict(value))
+                            }
+                            json_value, value = user_input
+
+                            if value in enum_values:
+                                user_input = None, _('Kein Wert')
+
+                        form.fields['value'] = user_input_field(
+                            _('Wert auswählen oder eintragen'),
+                            user_input,
+                            choices=list(choices.items()) + self.ref_choices
                         )
+                    else:
+                        if choices:
+                            form.fields['value'] = DynamicChoiceField(
+                                widget=BaseSelect(
+                                    '',
+                                    choices=list(choices.items())
+                                )
+                            )
 
     def get_ref_choices(self, kw_call: KeywordCall):
         ref_choices = get_keyword_parameters(kw_call) + get_prev_return_values(kw_call)
