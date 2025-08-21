aafeb27ef72b3d162ff858c3655d9258ad0d466e
Marduk Bolanos <marduk.bolanos@imbus.de>
2025-08-21 13:40:03 +0200

    RichTextField: Define the read-only field for the documentation instead of setting it dynamically at runtime

--- src/keyta/admin/field_documentation.py
+++ src/keyta/admin/field_documentation.py
@@ -1,3 +1,4 @@
+from django.contrib import admin
 from django.db import models
 from django.utils.safestring import mark_safe
 from django.utils.translation import gettext_lazy as _
@@ -14,33 +15,26 @@ class RichTextField:
 
     def get_fields(self, request, obj=None):
         if self.show_readonly(request, obj):
-            return self.fields + [self.readonly_field]
+            return self.fields + ['readonly_documentation']
 
         return self.fields + [self.field]
-        
+
+    @admin.display(description=_('Dokumentation'))
+    def readonly_documentation(self, obj):
+        return mark_safe(getattr(obj, self.field))
+
     def get_readonly_fields(self, request, obj=None):
-        def readonly_field(obj):
-            return mark_safe(getattr(obj, self.field))
-
-        readonly_field.short_description = _(self.field.capitalize())
-
         if self.show_readonly(request, obj):
-            setattr(self, self.readonly_field, readonly_field)
-
             return [
-                readonly_field if readonly_field != self.field else self.readonly_field
+                readonly_field if readonly_field != self.field else 'readonly_documentation'
                 for readonly_field in self.readonly_fields
             ]
 
         return self.readonly_fields
 
-    @property
-    def readonly_field(self):
-        return 'readonly_' + self.field
-
     def show_readonly(self, request, obj):
         return (
-            self.field in self.readonly_fields or 
+            self.field in self.readonly_fields or
             not self.has_change_permission(request, obj)
         )
 
