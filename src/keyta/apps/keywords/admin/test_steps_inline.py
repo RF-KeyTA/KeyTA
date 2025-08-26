7c4b87e89ea62fdff9c139277ef0920cd106cbd6
Marduk Bolanos <marduk.bolanos@imbus.de>
2025-08-26 11:12:59 +0200

    TestStepsInline: Do not override the formset

--- src/keyta/apps/keywords/admin/test_steps_inline.py
+++ src/keyta/apps/keywords/admin/test_steps_inline.py
@@ -1,10 +1,5 @@
 from keyta.admin.base_inline import SortableTabularInline
 from keyta.admin.field_delete_related_instance import DeleteRelatedField
-from keyta.apps.keywords.models import Keyword
-from keyta.apps.resources.models import ResourceImport
-from keyta.apps.testcases.models import TestCase
-from keyta.apps.windows.models import Window
-from keyta.widgets import quick_change_widget
 
 from ..forms import TestStepsForm, TestStepsFormset
 from ..models import KeywordCall
@@ -26,39 +21,3 @@ class TestStepsInline(
     def get_fields(self, request, obj=None):
         *fields, delete = super().get_fields(request, obj)
         return [*fields, 'variable', delete]
-
-    def get_formset(self, request, obj=None, **kwargs):
-        testcase: TestCase = obj
-        systems = testcase.systems.all()
-        windows = (
-            Window.objects
-            .filter(systems__in=systems)
-            .distinct()
-        )
-        resource_ids = (
-            ResourceImport.objects
-            .filter(window__in=windows)
-            .values_list('resource')
-            .distinct()
-        )
-
-        formset = super().get_formset(request, obj, **kwargs)
-
-        window_field = formset.form.base_fields['window']
-        window_field.queryset = windows
-        window_field.widget = quick_change_widget(window_field.widget)
-
-        to_keyword_field = formset.form.base_fields['to_keyword']
-        to_keyword_field.queryset = (
-            Keyword.objects.sequences().filter(systems__in=systems) |
-            Keyword.objects.filter(resource__in=resource_ids)
-        ).distinct().order_by('name')
-
-        variable_field = formset.form.base_fields['variable']
-        variable_field.queryset = (
-            variable_field.queryset
-            .filter(systems__in=systems)
-        ).distinct().order_by('name')
-        variable_field.widget = quick_change_widget(variable_field.widget)
-
-        return formset
