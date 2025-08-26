08ef4f1179dffbd94fd11cd52a8e20d2ecde67bd
Marduk Bolanos <marduk.bolanos@imbus.de>
2025-08-26 10:47:36 +0200

    TestStepsForm: Do not define custom widgets that override the queryset

--- src/keyta/apps/keywords/forms/test_steps_form.py
+++ src/keyta/apps/keywords/forms/test_steps_form.py
@@ -1,30 +1,12 @@
 from django import forms
 from django.utils.translation import gettext_lazy as _
 
-from keyta.apps.keywords.models.keyword import Keyword
-from keyta.apps.resources.models.resource_import import ResourceImport
 from keyta.apps.variables.models import Variable
 from keyta.apps.windows.models import Window
 from keyta.widgets import ModelSelect2AdminWidget
 
-from ..models import KeywordCall
+from ..models import Keyword, KeywordCall
 from .steps_form import StepsForm
-
-
-class SequenceSelectWidget(ModelSelect2AdminWidget):
-    def filter_queryset(self, request, term, queryset=None, **dependent_fields):
-        window_id = dependent_fields['windows']
-        sequences = Keyword.objects.sequences().filter(windows__in=[window_id]).distinct()
-        resource_ids = ResourceImport.objects.filter(window_id=window_id).values_list('resource')
-        
-        # The QuerySet returned by the super class is unique
-        # A unique QuerySet can only be combined with a unique QuerySet
-        return sequences | Keyword.objects.filter(resource__in=resource_ids).distinct()
-
-
-class VariableSelectWidget(ModelSelect2AdminWidget):
-    def filter_queryset(self, request, term, queryset=None, **dependent_fields):
-        return super().filter_queryset(request, term, queryset, **dependent_fields).filter(template='')
 
 
 TestStepsForm = forms.modelform_factory(
@@ -40,13 +22,13 @@ TestStepsForm = forms.modelform_factory(
             model=Window,
             search_fields=['name__icontains'],
         ),
-        'to_keyword': SequenceSelectWidget(
+        'to_keyword': ModelSelect2AdminWidget(
             placeholder=_('Sequenz auswählen'),
             model=Keyword,
             search_fields=['name__icontains'],
             dependent_fields={'window': 'windows'},
         ),
-        'variable': VariableSelectWidget(
+        'variable': ModelSelect2AdminWidget(
             placeholder=_('Referenzwert auswählen'),
             model=Variable,
             search_fields=['name__icontains'],
