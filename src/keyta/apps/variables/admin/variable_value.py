be21e5785a5f0984ae3cf8b88c9d79ee61715cde
Marduk Bolanos <marduk.bolanos@imbus.de>
2025-09-01 08:57:15 +0200

    Variable Value admin: A variable value cannot be deleted if it is being used in a keyword call parameter

--- src/keyta/apps/variables/admin/variable_value.py
+++ src/keyta/apps/variables/admin/variable_value.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
 
 from keyta.admin.base_admin import BaseAdmin
+from keyta.apps.keywords.models import KeywordCallParameter
 
 from ..models import VariableValue
 
 
 @admin.register(VariableValue)
 class VariableValueAdmin(BaseAdmin):
-    pass
+    def get_protected_objects(self, obj):
+        variable_value: VariableValue = obj
+
+        return (
+            list(KeywordCallParameter.objects.filter(value_ref__variable_value=variable_value))
+        )
