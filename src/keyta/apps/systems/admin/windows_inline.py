6671fe99ce94bb68d7055848676d896ca51e644b
Marduk Bolanos <marduk.bolanos@imbus.de>
2025-09-03 14:07:25 +0200

    System -> Windows inline: Sort the windows alphabetically

--- src/keyta/apps/systems/admin/windows_inline.py
+++ src/keyta/apps/systems/admin/windows_inline.py
@@ -20,5 +20,8 @@ class Windows(BaseTabularInline):
     verbose_name = _('Maske')
     verbose_name_plural = _('Masken')
 
+    def get_queryset(self, request):
+        return super().get_queryset(request).order_by('window__name')
+
     def has_change_permission(self, request, obj=None):
         return False
