ca23ddf0613b0f003d31626c0c2edb8b988515c2
Marduk Bolanos <marduk.bolanos@imbus.de>
2025-09-05 11:02:54 +0200

    SequenceStepAdmin: Added the conditions inline

--- src/keyta/apps/sequences/admin/sequence_step.py
+++ src/keyta/apps/sequences/admin/sequence_step.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 from keyta.apps.keywords.admin import KeywordCallAdmin
+from keyta.apps.keywords.admin.conditions_inline import ConditionsInline
 
 from ..models import SequenceStep
 
@@ -9,3 +10,15 @@ from ..models import SequenceStep
 class SequenceStepAdmin(KeywordCallAdmin):
     def change_view(self, request, object_id, form_url="", extra_context=None):
         return self.changeform_view(request, object_id, form_url, extra_context or {'show_delete': False})
+
+    def get_inlines(self, request, obj):
+        inlines = super().get_inlines(request, obj)
+        kw_call: SequenceStep = obj
+
+        if kw_call.from_keyword.parameters.exists() or kw_call.get_previous_return_values().exists():
+            for condition in kw_call.conditions.all():
+                condition.update_expected_value()
+
+            return inlines + [ConditionsInline]
+
+        return inlines
