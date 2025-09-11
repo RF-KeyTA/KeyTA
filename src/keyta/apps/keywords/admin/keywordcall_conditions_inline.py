1909e38a489f2022de2c8a57c25ddf9fa15fc2d4
Marduk Bolanos <marduk.bolanos@imbus.de>
2025-09-11 21:56:56 +0200

    ConditionsInline: Defined ConditionForm in order to validate the field 'expected_value'

--- src/keyta/apps/keywords/admin/keywordcall_conditions_inline.py
+++ src/keyta/apps/keywords/admin/keywordcall_conditions_inline.py
@@ -1,12 +1,28 @@
+from django import forms
+from django.utils.translation import gettext_lazy as _
+
 from keyta.admin.base_inline import TabularInlineWithDelete
 
 from ..forms import KeywordCallConditionFormset
+from ..json_value import JSONValue
 from ..models import KeywordCallCondition, KeywordCall
+
+
+class ConditionForm(forms.ModelForm):
+    def clean_expected_value(self):
+        expected_value = self.cleaned_data.get('expected_value')
+        json_value = JSONValue.from_json(expected_value)
+
+        if not json_value.user_input and not json_value.pk:
+            raise forms.ValidationError(_('Dieses Feld ist zwingend erforderlich.'))
+
+        return expected_value
 
 
 class ConditionsInline(TabularInlineWithDelete):
     model = KeywordCallCondition
     fields = ['value_ref', 'condition', 'expected_value']
+    form = ConditionForm
     formset = KeywordCallConditionFormset
 
     def has_add_permission(self, request, obj=None):
