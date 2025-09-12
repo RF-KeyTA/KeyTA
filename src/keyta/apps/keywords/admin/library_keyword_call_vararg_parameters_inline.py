e7a2e7c47888b8b0e7623eac96f6be4b1de287ae
Marduk Bolanos <marduk.bolanos@imbus.de>
2025-09-12 07:52:14 +0200

    Fixed an import

--- src/keyta/apps/keywords/admin/library_keyword_call_vararg_parameters_inline.py
+++ src/keyta/apps/keywords/admin/library_keyword_call_vararg_parameters_inline.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.utils.translation import gettext_lazy as _
 
-from admin.base_inline import TabularInlineWithDelete
+from keyta.admin.base_inline import TabularInlineWithDelete
 
 from ..forms import LibraryKeywordCallVarargFormset
 from ..models import (
