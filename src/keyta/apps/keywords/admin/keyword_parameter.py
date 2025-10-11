11e367655c75717f32964f3a288a42041880c92f
Marduk Bolanos <marduk.bolanos@imbus.de>
2025-10-11 13:16:41 +0200

    A keyword parameter cannot be deleted if a keyword is called more than once

--- src/keyta/apps/keywords/admin/keyword_parameter.py
+++ src/keyta/apps/keywords/admin/keyword_parameter.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.db.models import Q
 
 from keyta.admin.base_admin import BaseAdmin
 
@@ -9,13 +10,13 @@ from ..models import KeywordParameter, KeywordCallParameter
 class KeywordParameterAdmin(BaseAdmin):
     def get_protected_objects(self, obj):
         kw_parameter: KeywordParameter = obj
-
-        return list(
+        uses = (
             KeywordCallParameter.objects
-            .filter(value_ref__kw_param=kw_parameter)
-            .exclude(keyword_call__execution__isnull=False)
-        ) + list(
-            KeywordCallParameter.objects
-            .filter(parameter=kw_parameter)
+            .filter(Q(value_ref__kw_param=kw_parameter) | Q(parameter=kw_parameter))
             .exclude(keyword_call__execution__isnull=False)
         )
+
+        if uses.count() > 1:
+            return list(uses)
+
+        return []
