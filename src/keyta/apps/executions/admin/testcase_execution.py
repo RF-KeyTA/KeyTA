f40ab2ad25906be6afb96c53fe80b0b0cb93141d
Marduk Bolanos <marduk.bolanos@imbus.de>
2025-11-07 13:46:45 +0100

    TestCaseExecution -> TestSetupInline: The first setup step cannot be changed by the user

--- src/keyta/apps/executions/admin/testcase_execution.py
+++ src/keyta/apps/executions/admin/testcase_execution.py
@@ -1,13 +1,25 @@
 from django.contrib import admin
 from django.utils.translation import gettext_lazy as _
 
-from keyta.apps.executions.admin.execution import ExecutionAdmin
-
+from ..forms import SetupTeardownFormset
 from ..models import TestCaseExecution
+from .execution import ExecutionAdmin
 from .setup_teardown_inline import SetupInline, TeardownInline
 
 
+class TestSetupTeardownFormset(SetupTeardownFormset):
+    def add_fields(self, form, index):
+        super().add_fields(form, index)
+
+        if index == 0:
+            enabled_field = form.fields['enabled']
+            enabled_field.disabled = True
+            to_keyword_field = form.fields['to_keyword']
+            to_keyword_field.disabled = True
+
+
 class TestSetupInline(SetupInline):
+    formset = TestSetupTeardownFormset
     max_num = 2
     verbose_name_plural = _('Testvorbereitung')
 
