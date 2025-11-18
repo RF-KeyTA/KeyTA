82803692ee8462f90c99fa34568668fca08c4b73
Marduk Bolanos <marduk.bolanos@imbus.de>
2025-11-18 13:33:59 +0100

    AbstractBaseModel -> get_admin_url now has the optional parameter 'absolute'

--- src/keyta/models/base_model.py
+++ src/keyta/models/base_model.py
@@ -1,5 +1,6 @@
 from abc import ABCMeta
 
+from django.conf import settings
 from django.db import models
 from django.template.defaultfilters import slugify
 from django.urls import reverse
@@ -10,9 +11,14 @@ class AbstractModelMeta(ABCMeta, type(models.Model)):
 
 
 class AbstractBaseModel(models.Model, metaclass=AbstractModelMeta):
-    def get_admin_url(self, app=None):
+    def get_admin_url(self, app=None, absolute=False):
         app_model = (app or self._meta.app_label, self._meta.model_name)
-        return reverse('admin:%s_%s_change' % app_model, args=(self.pk,))
+        url = reverse('admin:%s_%s_change' % app_model, args=(self.pk,))
+
+        if absolute:
+            return settings.BASE_URL + url
+
+        return url
 
     def get_delete_url(self):
         app_model = (self._meta.app_label, self._meta.model_name)
