33bebc5e2771c57f2a9071e28138a2ea0b4fc97b
Marduk Bolanos <marduk.bolanos@imbus.de>
2026-01-09 08:38:43 +0100

    Keyword parameters can be deleted when the keyword is in use

--- src/keyta/apps/keywords/admin/keyword_parameter.py
+++ src/keyta/apps/keywords/admin/keyword_parameter.py
@@ -1,22 +1,10 @@
 from django.contrib import admin
-from django.db.models import Q
 
 from keyta.admin.base_admin import BaseAdmin
 
-from ..models import KeywordParameter, KeywordCallParameter
+from ..models import KeywordParameter
 
 
 @admin.register(KeywordParameter)
 class KeywordParameterAdmin(BaseAdmin):
-    def get_protected_objects(self, obj):
-        kw_parameter: KeywordParameter = obj
-        uses = (
-            KeywordCallParameter.objects
-            .filter(Q(value_ref__kw_param=kw_parameter) | Q(parameter=kw_parameter))
-            .exclude(keyword_call__execution__isnull=False)
-        )
-
-        if uses.count() > 1:
-            return list(uses)
-
-        return []
+    pass
