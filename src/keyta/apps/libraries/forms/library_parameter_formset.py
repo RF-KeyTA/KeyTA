d5bda643c7133585c36034d3f66b576b3e0a5058
Marduk Bolanos <marduk.bolanos@imbus.de>
2026-01-23 16:47:20 +0100

    LibraryParameterFormSet.add_fields: Refactored the code to simplify it

--- src/keyta/apps/libraries/forms/library_parameter_formset.py
+++ src/keyta/apps/libraries/forms/library_parameter_formset.py
@@ -18,23 +18,17 @@ class LibraryParameterFormSet(forms.BaseInlineFormSet):
             library_parameter: LibraryParameter = import_param.library_parameter
 
         if library_parameter.pk:
+            choices = []
+            enable_user_input = False
             typedocs = library_parameter.library.get_typedocs()
 
             if type_list := library_parameter.get_typedoc():
                 if type_list == ['bool']:
-                    choices = [
+                    choices.extend([
                         ('True', 'True'),
                         ('False', 'False')
-                    ]
-
-                    form.fields['value'].widget = BaseSelect(
-                        '',
-                        choices=choices
-                    )
+                    ])
                 else:
-                    choices = []
-                    enable_user_input = False
-
                     for type_ in type_list:
                         if type_ == 'None':
                             choices.append(('${None}', 'None'))
@@ -50,37 +44,35 @@ class LibraryParameterFormSet(forms.BaseInlineFormSet):
                                 for item in typedoc['items']:
                                     if item.lower() not in {'true', 'false'}:
                                         choices.append((item, item))
-                            elif typedoc['type'] == 'TypedDict':
-                                enable_user_input = True
                             else:
                                 enable_user_input = True
                         else:
                             enable_user_input = True
 
-                    if enable_user_input:
-                        if import_param:
-                            user_input = import_param.value, import_param.value
-                        else:
-                            user_input = library_parameter.value, library_parameter.value
+            if enable_user_input:
+                if import_param:
+                    user_input = import_param.value, import_param.value
+                else:
+                    user_input = library_parameter.value, library_parameter.value
 
-                        if user_input == ('${None}', '${None}'):
-                            user_input = ('${None}', 'None')
+                if user_input == ('${None}', '${None}'):
+                    user_input = ('${None}', 'None')
 
-                        if user_input in set(choices):
-                            user_input = None, _('Kein Wert')
+                if user_input in set(choices):
+                    user_input = None, _('Kein Wert')
 
-                        form.fields['value'].widget = BaseSelect(
-                            '',
-                            choices=(
-                                [(_('Eingabe'), [user_input])] +
-                                choices
-                            ),
-                            attrs={
-                                'data-tags': 'true'
-                            }
-                        )
-                    else:
-                        form.fields['value'].widget = BaseSelect(
-                            '',
-                            choices=choices
-                        )
+                form.fields['value'].widget = BaseSelect(
+                    '',
+                    choices=(
+                        [(_('Eingabe'), [user_input])] +
+                        choices
+                    ),
+                    attrs={
+                        'data-tags': 'true'
+                    }
+                )
+            else:
+                form.fields['value'].widget = BaseSelect(
+                    '',
+                    choices=choices
+                )
