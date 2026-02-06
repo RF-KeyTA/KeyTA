830641300c97044bdb6ae4f8f9a133908000f128
Marduk Bolanos <marduk.bolanos@imbus.de>
2026-02-06 17:08:47 +0100

    Sequence admin -> Uses inline: Show each Test Case only once

--- src/keyta/apps/sequences/admin/uses_inline.py
+++ src/keyta/apps/sequences/admin/uses_inline.py
@@ -5,6 +5,8 @@ from keyta.admin.base_inline import BaseTabularInline
 from keyta.apps.testcases.models import TestStep
 from keyta.widgets import link
 
+from ..models import Sequence
+
 
 class UsesInline(BaseTabularInline):
     model = TestStep
@@ -14,7 +16,16 @@ class UsesInline(BaseTabularInline):
     verbose_name_plural = _('Verwendungen')
 
     def get_queryset(self, request):
-        return super().get_queryset(request).order_by('testcase__name')
+        sequence_id = request.resolver_match.kwargs['object_id']
+        sequence = Sequence.objects.get(id=sequence_id)
+        testcase_to_pk = dict(
+            super().get_queryset(request)
+            .filter(to_keyword=sequence)
+            .values_list('testcase', 'pk')
+        )
+        pks = list(testcase_to_pk.values())
+
+        return super().get_queryset(request).filter(pk__in=pks).order_by('testcase__name')
 
     def has_add_permission(self, request, obj):
         return False
