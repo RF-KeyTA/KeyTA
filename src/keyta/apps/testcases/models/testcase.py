0d8616282e55a34251d006986aaf463dd282d178
Marduk Bolanos <marduk.bolanos@imbus.de>
2026-04-10 14:05:46 +0200

    TestCase.to_robot: Do not add query params to the URL in the documentation

--- src/keyta/apps/testcases/models/testcase.py
+++ src/keyta/apps/testcases/models/testcase.py
@@ -230,7 +230,7 @@ class TestCase(CloneMixin, AbstractBaseModel):
         if include_doc:
             documentation = HTML2Text.parse(self.documentation)
         else:
-            documentation = self.get_admin_url(absolute=True) + '?steps_tab'
+            documentation = self.get_admin_url(absolute=True)
 
         parameter_values: dict[int, StepParameterValues] = {}
 
