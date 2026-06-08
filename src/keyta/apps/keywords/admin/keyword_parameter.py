bebc959024c71ce743268aa7978bdb79996f2bcb
Marduk Bolanos <marduk.bolanos@imbus.de>
2026-06-08 14:01:00 +0200

    A Keyword Parameter cannot be deleted if it is being used
    (cherry picked from commit 9a8bea210d101790a92f646462b04df8e5cc8896)

--- src/keyta/apps/keywords/admin/keyword_parameter.py
+++ src/keyta/apps/keywords/admin/keyword_parameter.py
@@ -2,9 +2,14 @@ from django.contrib import admin
 
 from keyta.admin.base_admin import BaseAdmin
 
-from ..models import KeywordParameter
+from ..models import KeywordParameter, KeywordCallParameter
 
 
 @admin.register(KeywordParameter)
 class KeywordParameterAdmin(BaseAdmin):
-    pass
+    def get_protected_objects(self, parameter: KeywordParameter):
+        return (
+            KeywordCallParameter.objects
+            .filter(keyword_call__from_keyword=parameter.keyword, value_ref__kw_param=parameter)
+            .distinct()
+        )
