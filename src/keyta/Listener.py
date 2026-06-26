1b877b0ea1abf01752cac0956d0064e20198f1c4
Marduk Bolanos <marduk.bolanos@imbus.de>
2026-06-26 16:47:31 +0200

    Listener: If a RoboSAPiens keyword fails take a screenshot and embed it in the log

--- src/keyta/Listener.py
+++ src/keyta/Listener.py
@@ -1,13 +1,13 @@
 import os
 import re
 
-from robot import running
-from robot.result import TestCase
+from robot import result, running
 from robot.api import logger
 from robot.api.interfaces import ListenerV3
+from robot.libraries.BuiltIn import BuiltIn
 
 
-# On windows, calling os.system("") makes ANSI escape sequences
+# On Windows, calling os.system("") makes ANSI escape sequences
 # get processed correctly
 os.system("")
 
@@ -17,8 +17,29 @@ def escape_ansi(line):
     return ansi_escape.sub('', line)
 
 
+def get_robosapiens():
+    library = 'RoboSAPiens'
+
+    try:
+        return BuiltIn().get_library_instance(library)
+    except:
+        pass
+
+    try:
+        return BuiltIn().get_library_instance(library + '.DE')
+    except:
+        pass
+
+    return None
+
+
 class Listener(ListenerV3):
-    def end_test(self, data: running.TestCase, result: TestCase):
+    def end_library_keyword(self, data: running.Keyword, implementation: running.LibraryKeyword, result: result.Keyword):
+        if result.failed and implementation.full_name.startswith('RoboSAPiens'):
+            if robosapiens := get_robosapiens():
+                robosapiens.save_screenshot('LOG')
+
+    def end_test(self, data: running.TestCase, result: result.TestCase):
         if result.failed:
             message = result.message
             logger.error(escape_ansi(message))
