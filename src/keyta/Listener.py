39490dec5c6a970ef87c3c864c05f3dd3f1064ad
Marduk Bolanos <marduk.bolanos@imbus.de>
2026-06-26 16:50:44 +0200

    Listener: If Browser library is present, patch the presenter mode in order to deactivate the selector picker

--- src/keyta/Listener.py
+++ src/keyta/Listener.py
@@ -1,5 +1,7 @@
 import os
 import re
+from functools import wraps
+from time import sleep
 
 from robot import result, running
 from robot.api import logger
@@ -15,6 +17,37 @@ os.system("")
 def escape_ansi(line):
     ansi_escape = re.compile(r'(?:\x1B[@-_]|[\x80-\x9F])[0-?]*[ -/]*[@-~]')
     return ansi_escape.sub('', line)
+
+
+def patch_presenter_mode():
+    try:
+        import Browser.base
+
+        presenter_mode = Browser.base.LibraryComponent.presenter_mode
+
+        @wraps(presenter_mode)
+        def wrapper(self, selector, strict):
+            selector = self.resolve_selector(selector)
+            if self.library.presenter_mode:
+                mode = self.get_presenter_mode
+                try:
+                    self.library.scroll_to_element(selector)
+                    self.library.highlight_elements(
+                        selector,
+                        duration=mode["duration"],
+                        width=mode["width"],
+                        style=mode["style"],
+                        color=mode["color"],
+                    )
+                except Exception as error:
+                    logger.debug(f"On presenter mode supress {error}")
+                else:
+                    sleep(mode["duration"].seconds)
+            return selector
+
+        Browser.base.LibraryComponent.presenter_mode = wrapper
+    except:
+        pass
 
 
 def get_robosapiens():
@@ -43,3 +76,6 @@ class Listener(ListenerV3):
         if result.failed:
             message = result.message
             logger.error(escape_ansi(message))
+
+    def start_suite(self, data: running.TestSuite, result: result.TestSuite):
+        patch_presenter_mode()
